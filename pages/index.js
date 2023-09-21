fa05f29ecb38a79197f70831d6efb6b778bafca7
Sarthak Mishra <124512392+sarthakmishra459@users.noreply.github.com>
2023-09-21 19:00:22 +0530

    Crowdfunding Dapp

--- pages/index.js
+++ pages/index.js
@@ -333,7 +333,7 @@ export default function Home({ campaigns }) {
             For any queries raise an issue on{" "}
             <Link
               color="blue.500"
-              href="https://github.com/harsh242/betterfund-crowdfunding-in-blockchain/issues"
+              href="https://github.com/sarthakmishra459/IncognitoHand/issues"
               isExternal
             >
               the Github Repo <ExternalLinkIcon mx="2px" />
